3561119b34f3c6d97e6e422040c7c04740668e79
surajhgh <143310572+surajhgh@users.noreply.github.com>
2026-04-13 12:05:05 +0545

    n

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+    verification: {
+    google: "-Tpbor083mZ32WeUusb6pJFswmlgZNM10QMllkyWY4A",
+  },
   title: 'Nobel Environment Academy | Tilottama-15, Kotihawa, Nepal',
   description:
     'Montessori and secondary school in Kotihawa, Rupandehi, Nepal. Quality education from pre-school to Grade 10.'
